bb52fba125ffd13263134242cf45c38f26d280cc
Benson Ndiwa (Techno Brain Global FZ LLC) <v-bndiwa@microsoft.com>
2023-08-22 20:55:40 +0300

    longtail

--- controllers/LongtailContoller.js
+++ controllers/LongtailContoller.js
@@ -8,9 +8,50 @@ const stopwords = require('natural').stopwords;
 
 let app =  express.Router();
 
+// async function scrapeKeywords(url, targetKeyword) {
+//     try {
+//       const response = await axios.get(url);
+//       const html = response.data;
+//       const $ = cheerio.load(html);
+  
+//       const allText = $('body').text();
+//       const words = allText.split(/\s+/); // Split text into words
+  
+//       const keywordMap = new Map();
+  
+//       // Count occurrences of words containing the target keyword as a substring
+//       words.forEach(word => {
+//         const sanitizedWord = word.toLowerCase().replace(/[^\w\s]/gi, ''); // Remove non-alphanumeric characters
+//         if (sanitizedWord.includes(targetKeyword) && sanitizedWord !== targetKeyword) {
+//           if (keywordMap.has(sanitizedWord)) {
+//             keywordMap.set(sanitizedWord, keywordMap.get(sanitizedWord) + 1);
+//           } else {
+//             keywordMap.set(sanitizedWord, 1);
+//           }
+//         }
+//       });
+  
+//       const sortedKeywords = Array.from(keywordMap.entries()).sort((a, b) => b[1] - a[1]);
+  
+//       console.log(`Long-tail keywords related to "${targetKeyword}":`);
+//       sortedKeywords.forEach(([keyword, count]) => {
+//         console.log(`${keyword} (${count} occurrences)`);
+//       });
+//     } catch (error) {
+//       console.error('Error scraping the web page:', error);
+//     }
+//   }
+  
+//   const targetUrl = 'https://moz.com';
+  
+
 app.get('/longtail/:keyword', (req, res)=>{
+        //scrapeKeywords(targetUrl, req.params.keyword);
+
         // Define the URL to scrape
-        const url = 'https://moz.com/help';
+        const url = 'https://www.tutorialspoint.com';
+
+        let searchTerm = req.params.keyword;
         
         // Initialize TF-IDF instance
         const tfidf = new TfIdf();
@@ -23,6 +64,8 @@ app.get('/longtail/:keyword', (req, res)=>{
             const $ = cheerio.load(scrapedData);
 
             const textContent = $('p').text();
+
+            console.log(textContent);
 
             // Tokenize the target keyword
             let tokenizer = new natural.WordTokenizer();
@@ -37,11 +80,35 @@ app.get('/longtail/:keyword', (req, res)=>{
             // Calculate TF-IDF for the target keyword
             const keywords = [];
 
-            tfidf.listTerms(0).forEach(term =>{
-                if(term.term.length >= 4){
-                    keywords.push(term.term);
+            // tfidf.listTerms(0).forEach(term =>{
+            //     if(term.term.length >= 4 && term.term.includes(searchTerm)){
+            //         keywords.push(term.term);
+            //     }
+            // })
+
+            // const completeLongtailKeyword = [];
+            
+            // for(let i = 0; i < keywords.length; i++){
+            //     for(let j = i + 1; j < keywords.length; j++){
+            //         const phrase = `${keywords[i]} ${keywords[j]}`;
+            //         completeLongtailKeyword.push(phrase);
+            //     }
+            // }
+            const MIN_KEYWORD_LENGTH = 3;
+            let currentKeyword = '';
+
+            for (const word of filteredTokens) {
+                if (word.length >= MIN_KEYWORD_LENGTH) {
+                  if (currentKeyword === '') {
+                    currentKeyword = word;
+                  } else {
+                    currentKeyword += ' ' + word;
+                  }
+                } else if (currentKeyword !== '') {
+                  keywords.push(currentKeyword);
+                  currentKeyword = '';
                 }
-            })
+              }
 
             res.json(keywords);
         })
