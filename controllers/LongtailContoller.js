f0e8cf0f16b7d1020990a8ff889de7fcf5f7b2e0
Benson Ndiwa <bnkimtai@gmail.com>
2023-08-26 22:21:46 +0300

    Longtail keywords algorithm making sense

--- controllers/LongtailContoller.js
+++ controllers/LongtailContoller.js
@@ -7,47 +7,9 @@ const cheerio = require('cheerio');
 const stopwords = require('natural').stopwords;
 
 let app =  express.Router();
-
-// async function scrapeKeywords(url, targetKeyword) {
-//     try {
-//       const response = await axios.get(url);
-//       const html = response.data;
-//       const $ = cheerio.load(html);
-  
-//       const allText = $('body').text();
-//       const words = allText.split(/\s+/); // Split text into words
-  
-//       const keywordMap = new Map();
-  
-//       // Count occurrences of words containing the target keyword as a substring
-//       words.forEach(word => {
-//         const sanitizedWord = word.toLowerCase().replace(/[^\w\s]/gi, ''); // Remove non-alphanumeric characters
-//         if (sanitizedWord.includes(targetKeyword) && sanitizedWord !== targetKeyword) {
-//           if (keywordMap.has(sanitizedWord)) {
-//             keywordMap.set(sanitizedWord, keywordMap.get(sanitizedWord) + 1);
-//           } else {
-//             keywordMap.set(sanitizedWord, 1);
-//           }
-//         }
-//       });
-  
-//       const sortedKeywords = Array.from(keywordMap.entries()).sort((a, b) => b[1] - a[1]);
-  
-//       console.log(`Long-tail keywords related to "${targetKeyword}":`);
-//       sortedKeywords.forEach(([keyword, count]) => {
-//         console.log(`${keyword} (${count} occurrences)`);
-//       });
-//     } catch (error) {
-//       console.error('Error scraping the web page:', error);
-//     }
-//   }
-  
-//   const targetUrl = 'https://moz.com';
   
 
-app.get('/longtail/:keyword', (req, res)=>{
-        //scrapeKeywords(targetUrl, req.params.keyword);
-
+app.get('/longtail', (req, res)=>{
         // Define the URL to scrape
         const url = 'https://moz.com/help';
 
@@ -77,22 +39,8 @@ app.get('/longtail/:keyword', (req, res)=>{
 
             // Calculate TF-IDF for the target keyword
             const keywords = [];
-
-            // tfidf.listTerms(0).forEach(term =>{
-            //     if(term.term.length >= 4 && term.term.includes(searchTerm)){
-            //         keywords.push(term.term);
-            //     }
-            // })
-
-            // const completeLongtailKeyword = [];
             
-            // for(let i = 0; i < keywords.length; i++){
-            //     for(let j = i + 1; j < keywords.length; j++){
-            //         const phrase = `${keywords[i]} ${keywords[j]}`;
-            //         completeLongtailKeyword.push(phrase);
-            //     }
-            // }
-            const MIN_KEYWORD_LENGTH = 3;
+            const MIN_KEYWORD_LENGTH = 4;
             let currentKeyword = '';
 
             for (const word of filteredTokens) {
@@ -107,8 +55,18 @@ app.get('/longtail/:keyword', (req, res)=>{
                   currentKeyword = '';
                 }
               }
+            
+            //Extract keywords with a minimum length of three words 
+            const sensibleKeywords = [];
+            
+            keywords.map(phrase => {
+              const words = phrase.split(' ');
+                if(words.length > 3){
+                  sensibleKeywords.push(phrase);
+                }
+            })
 
-            res.json(keywords);
+            res.json(sensibleKeywords);
         })
         .catch(error => {
             console.error('Error scraping data:', error);
