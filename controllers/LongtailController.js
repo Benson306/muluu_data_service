5241d9d99989bad1730e2507774a186b4e682ab0
Benson306 <bnkimtai@gmail.com>
2023-09-16 09:27:41 +0300

    longtail keyword algorithm improved

--- controllers/LongtailController.js
+++ controllers/LongtailController.js
@@ -18,68 +18,57 @@ function getLongtailFromScrappedDataInDb(scrapedData){
       // Initialize TF-IDF instance
       const tfidf = new TfIdf();
 
+      // Tokenize the target keyword
+      let tokenizer = new natural.WordTokenizer();
+
       const $ = cheerio.load(scrapedData);
 
       const textContent = $('p').text();
 
-      // Tokenize the target keyword
-      let tokenizer = new natural.WordTokenizer();
-
-      //const tokens = tokenizer.tokenize(textContent);
-
-      const filteredTokens = tokenizer.tokenize(textContent);
-
-      //Remove stopwords
-      //const filteredTokens = tokens.filter(token => !stopwords.includes(token.toLowerCase()));
-
+      //Tokenize document
+      const tokens = tokenizer.tokenize(textContent);
+      
       // Add documents (text) to the TF-IDF instance
-      tfidf.addDocument(filteredTokens);
+      tfidf.addDocument(tokens);
 
-      // Calculate TF-IDF for the target keyword
-      const keywords = [];
+      let paragraphs = [];
 
-      const MIN_KEYWORD_LENGTH = 3;
-      let currentKeyword = '';
-
-      for (const word of filteredTokens) {
-          if (word.length >= MIN_KEYWORD_LENGTH) {
-            if (currentKeyword === '') {
-              currentKeyword = word;
-            } else {
-              currentKeyword += ' ' + word;
-            }
-          } else if (currentKeyword !== '') {
-            keywords.push(currentKeyword);
-            currentKeyword = '';
-          }
+      $('p').each((index, element)=>{
+        const paragraphText = $(element).text().trim();
+        if(paragraphText){
+          paragraphs.push(paragraphText)
         }
+      })
+      
+      // Filter Keywords with numbers
+      //const sentences = paragraphs.filter(keyword => !/\d/.test(keyword));
 
-        const filteredSensibleKeywords = keywords.filter(keyword => !/\d/.test(keyword));
 
-      //Extract keywords with a minimum length of three words 
-      const sensibleKeywords = [];
-
-      filteredSensibleKeywords.forEach(phrase => {
-        const words = phrase.split(' ');
-          if(words.length > 3 && words.length < 12){
-
-            let totalTfIdfScore = 0;
-
-            words.forEach(token => {
-              let score = tfidf.tfidfs(token, (i, measure) => measure);
-              totalTfIdfScore += Number(score[0].toFixed(3));
-            });
-
-            sensibleKeywords.push({ longtail_keyword: phrase, score: Number(totalTfIdfScore.toFixed(3)) });
-
-            //sensibleKeywords.push(phrase);
+      let phrases = [];
+      // Get phrases with more than 3 words and less 
+      paragraphs.forEach(sentence => {
+        const words = sentence.split(' ');
+           if(words.length > 3 && words.length < 12){
+            const words = sentence.split(' ');
+            if(words.length > 3 && words.length < 12){
+                  let totalTfIdfScore = 0;
+      
+                  words.forEach(token => {
+                    let score = tfidf.tfidfs(token, (i, measure) => measure);
+                    totalTfIdfScore += Number(score[0].toFixed(3));
+                  });
+      
+                  phrases.push({ longtail_keyword: sentence, score: Number(totalTfIdfScore.toFixed(3)) });
+                }
           }
       })
 
       // Sort keywords based on TF-IDF scores
-      sensibleKeywords.sort((a, b) => b.score - a.score);
+      phrases.sort((a, b) => b.score - a.score);
+
+
       //Save to DB
-      sensibleKeywords.forEach((keyword)=>{
+      phrases.forEach((keyword)=>{
         if(keyword.score > 4){
           //Check for duplicates
           LongtailModel.find({ longtail_keyword: { $regex : keyword.longtail_keyword, $options: 'i'}})
@@ -99,9 +88,10 @@ function getLongtailFromScrappedDataInDb(scrapedData){
             }
             
           })
-
         }
       })
+
+      
 }
 
 function run_longtail_scrapper(){
@@ -171,3 +161,87 @@ app.get('/longtail/:keyword', (req, res)=>{
 })
 
 module.exports = app;
+
+
+// const textContent = $('p').text();
+
+      // // Tokenize the target keyword
+      // let tokenizer = new natural.WordTokenizer();
+
+      // //const tokens = tokenizer.tokenize(textContent);
+
+      // const filteredTokens = tokenizer.tokenize(textContent);
+
+      // //Remove stopwords
+      // //const filteredTokens = tokens.filter(token => !stopwords.includes(token.toLowerCase()));
+
+      // // Add documents (text) to the TF-IDF instance
+      // tfidf.addDocument(filteredTokens);
+
+      // // Calculate TF-IDF for the target keyword
+      // const keywords = [];
+
+      // const MIN_KEYWORD_LENGTH = 3;
+      // let currentKeyword = '';
+
+      // for (const word of filteredTokens) {
+      //     if (word.length >= MIN_KEYWORD_LENGTH) {
+      //       if (currentKeyword === '') {
+      //         currentKeyword = word;
+      //       } else {
+      //         currentKeyword += ' ' + word;
+      //       }
+      //     } else if (currentKeyword !== '') {
+      //       keywords.push(currentKeyword);
+      //       currentKeyword = '';
+      //     }
+      //   }
+
+      //   const filteredSensibleKeywords = keywords.filter(keyword => !/\d/.test(keyword));
+
+      // //Extract keywords with a minimum length of three words 
+      // const sensibleKeywords = [];
+
+      // filteredSensibleKeywords.forEach(phrase => {
+      //   const words = phrase.split(' ');
+      //     if(words.length > 3 && words.length < 12){
+
+      //       let totalTfIdfScore = 0;
+
+      //       words.forEach(token => {
+      //         let score = tfidf.tfidfs(token, (i, measure) => measure);
+      //         totalTfIdfScore += Number(score[0].toFixed(3));
+      //       });
+
+      //       sensibleKeywords.push({ longtail_keyword: phrase, score: Number(totalTfIdfScore.toFixed(3)) });
+
+      //       //sensibleKeywords.push(phrase);
+      //     }
+      // })
+
+      // // Sort keywords based on TF-IDF scores
+      // sensibleKeywords.sort((a, b) => b.score - a.score);
+      // //Save to DB
+      // sensibleKeywords.forEach((keyword)=>{
+      //   if(keyword.score > 4){
+      //     //Check for duplicates
+      //     LongtailModel.find({ longtail_keyword: { $regex : keyword.longtail_keyword, $options: 'i'}})
+      //     .then((data)=>{
+          
+      //       if(data.length == 0){
+      //         //Save if there are no duplicates
+      //           LongtailModel(keyword).save()
+      //           .then(()=>{
+      //             console.log('Longtail Data Saved');
+      //           })
+      //           .catch(()=>{
+      //             console.log("Error in saving longtail keyword");
+      //           })
+      //       }else{
+      //         console.log('keyword not found')
+      //       }
+            
+      //     })
+
+      //   }
+      // })
