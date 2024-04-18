1ca2a8309b44b134b5c044951a6f22aad5852c56
Benson306 <bnkimtai@gmail.com>
2024-04-19 01:10:58 +0300

    Error handling

--- controllers/DomainController.js
+++ controllers/DomainController.js
@@ -8,45 +8,45 @@ const WebsiteTrafficModel = require('../models/WebsiteTrafficModel');
 const PageSEOModel = require('../models/PageSEOModel');
 const urlEncoded = bodyParser.urlencoded({ extended: false });
 
-app.post('/keywords_ranking_in_domain', urlEncoded, (req, res)=>{
-    let domain = req.body.domain;
-    let results = { }
-    KeywordsRankingInDomainModel.findOne({ domain : domain})
-    .then(data =>{
-        if(data){
-            res.json(data);
-        }else{
-            const request = unirest('GET', 'https://seo-website-ranking-keywords.p.rapidapi.com/');
-            request.query({
-                domain: domain,
-            });
-            request.headers({
-                'X-RapidAPI-Key': `${process.env.RAPID_API_KEY}`,
-                'X-RapidAPI-Host': 'seo-website-ranking-keywords.p.rapidapi.com'
-            });
-            request.end(function (response) {
-                if (response.error){
-                    res.status(400).json('Failed');
-                }else{
-                    results.domain = domain;
-                    if(response.body.keywords.length > 10){
-                        results.keywords = response.body.keywords.slice(1, 11);
-                    }else{
-                        results.keywords = response.body.keywords;
-                    }
+// app.post('/keywords_ranking_in_domain', urlEncoded, (req, res)=>{
+//     let domain = req.body.domain;
+//     let results = { }
+//     KeywordsRankingInDomainModel.findOne({ domain : domain})
+//     .then(data =>{
+//         if(data){
+//             res.json(data);
+//         }else{
+//             const request = unirest('GET', 'https://seo-website-ranking-keywords.p.rapidapi.com/');
+//             request.query({
+//                 domain: domain,
+//             });
+//             request.headers({
+//                 'X-RapidAPI-Key': `${process.env.RAPID_API_KEY}`,
+//                 'X-RapidAPI-Host': 'seo-website-ranking-keywords.p.rapidapi.com'
+//             });
+//             request.end(function (response) {
+//                 if (response.error){
+//                     res.status(400).json('Failed');
+//                 }else{
+//                     results.domain = domain;
+//                     if(response.body.keywords.length > 10){
+//                         results.keywords = response.body.keywords.slice(1, 11);
+//                     }else{
+//                         results.keywords = response.body.keywords;
+//                     }
     
-                    KeywordsRankingInDomainModel(results).save()
-                    .then(newData => {
-                        res.json(newData);
-                    })
-                    .catch(err => {
-                        res.status(400).json('failed');
-                    })
-                }
-            });
-        }
-    })
-})
+//                     KeywordsRankingInDomainModel(results).save()
+//                     .then(newData => {
+//                         res.json(newData);
+//                     })
+//                     .catch(err => {
+//                         res.status(400).json('failed');
+//                     })
+//                 }
+//             });
+//         }
+//     })
+// })
 
 function getAPIToken(callback){
     const request = unirest("POST", "https://app.boostramp.com/api/login.php")
@@ -75,24 +75,23 @@ app.post('/backlinks', urlEncoded, (req, res)=>{
             let request = unirest("POST", `https://app.boostramp.com/api/tools.php?token=${token}&func=getBacklinks`)
             request.field("domain", domain)
             request.end(function (response){
-                if (response.error) {
-                    res.status(500).json("Failed to fetch")
-                };
+                if (response.error || response.body.error) {
+                    res.status(500).json(`Failed to fetch ${response.error || response.body.error}`)
+                }else{
+                    const result = JSON.parse(response.body);
+
+                    const completeResult = {};
+                    completeResult.domain = domain;
+                    completeResult.result = result;
     
-                const result = JSON.parse(response.body);
-
-                const completeResult = {};
-                completeResult.domain = domain;
-                completeResult.result = result;
-
-                BacklinksModel(completeResult).save()
-                .then(newData => {
-                    res.json(newData);
-                })
-                .catch(err => {
-                    res.status(400).json('failed');
-                })
-                
+                    BacklinksModel(completeResult).save()
+                    .then(newData => {
+                        res.json(newData);
+                    })
+                    .catch(err => {
+                        res.status(400).json('failed');
+                    })
+                }
             })
 
             // const request = unirest('GET', 'https://seo-api-get-backlinks.p.rapidapi.com/backlinks.php');
@@ -153,8 +152,8 @@ app.post('/website_traffic', urlEncoded, async (req, res)=>{
             request.field("language_code", language_code)
             request.field("location_code", 2404)
             request.end(function (response){
-                if (response.error) {
-                    res.status(500).json("Failed to fetch")
+                if (response.error || response.body.error) {
+                    res.status(500).json(`Failed to fetch ${response.error || response.body.error}`)
                 }else{
                     const result = JSON.parse(response.body);
                 
@@ -194,9 +193,9 @@ app.post('/page_seo', urlEncoded, (req, res)=>{
             let request = unirest("POST", `https://app.boostramp.com/api/tools.php?token=${token}&func=pageSEOCheck`)
             request.field("url", domain)
             request.end(function (response){
-                if (response.error.body) {
-                    res.status(500).json("Failed to fetch")
-                }else{                
+                if (response.error || response.body.error) {
+                    res.status(500).json(`Failed to fetch ${response.error || response.body.error}`)
+                }else{               
                     const completeResult = {};
                     completeResult.domain = domain;
                     completeResult.result = response.body;
@@ -213,6 +212,79 @@ app.post('/page_seo', urlEncoded, (req, res)=>{
             })
         }
     })
+})
+
+app.post('/competitors_ranking', urlEncoded ,async (req, res)=>{
+    let domain = req.body.domain;
+    const token = await new Promise((resolve, reject) => {
+        getAPIToken((key) => {
+            const newToken = JSON.parse(key);
+            resolve(newToken.token);
+        });
+    });
+
+    let request = unirest("POST", `https://app.boostramp.com/api/tools.php?token=${token}&func=getCompetitorsRanking`)
+    request.field("country", 2404)
+    request.field("domain", domain)
+    request.end(function (response){
+        if (response.error || response.body.error) {
+            res.status(500).json(`Failed to fetch ${response.error || response.body.error}`)
+        }else{              
+            const completeResult = {};
+            completeResult.domain = domain;
+            completeResult.result = JSON.parse(response.body);
+
+            PageSEOModel(completeResult).save()
+            .then(newData => {
+                res.json(newData);
+            })
+            .catch(err => {
+                res.status(400).json('failed');
+            })
+        }
+            
+    })
+})
+
+app.post('/keywords_ranking_in_domain', urlEncoded , (req, res)=>{
+    let domain = req.body.domain;
+
+    KeywordsRankingInDomainModel.findOne({ domain : domain})
+    .then(async data =>{
+        if(data){
+            res.json(data);
+        }else{
+            const token = await new Promise((resolve, reject) => {
+                getAPIToken((key) => {
+                    const newToken = JSON.parse(key);
+                    resolve(newToken.token);
+                });
+            });
+        
+            let request = unirest("POST", `https://app.boostramp.com/api/tools.php?token=${token}&func=getWebsiteKeywords`)
+            request.field("country", 2404)
+            request.field("domain", domain)
+            request.end(function (response){
+                if (response.error || response.body.error) {
+                    res.status(500).json(`Failed to fetch ${response.error || response.body.error}`)
+                }else{               
+                    const completeResult = {};
+                    completeResult.domain = domain;
+                    console.log(response.body);
+                    completeResult.result = JSON.parse(response.body);
+
+                    KeywordsRankingInDomainModel(completeResult).save()
+                    .then(newData => {
+                        res.json(newData);
+                    })
+                    .catch(err => {
+                        res.status(400).json('failed');
+                    })
+                }  
+            })      
+        }
+    })
+    
 })
 
 module.exports = app;
